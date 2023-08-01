4f1de29ba062d4210807077bcab46189e418afb2
michaelvs.akseleran <michael.vincentius@akseleran.com>
2023-08-02 01:55:47 +0700

    Update Repo

--- Apps/app.py
+++ Apps/app.py
@@ -1,63 +1,54 @@
 import os
-from dotenv import load_dotenv
-import psycopg2
 import redis
+import psycopg2
+from flask import Flask, jsonify
+from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+app = Flask(__name__)
 
-# PostgreSQL configuration
-PG_HOST = os.getenv("PG_HOST")
-PG_PORT = os.getenv("PG_PORT")
-PG_USER = os.getenv("PG_USER")
-PG_PASSWORD = os.getenv("PG_PASSWORD")
-PG_DATABASE = os.getenv("PG_DATABASE")
+def check_redis_connection(host, port, password=None):
+    try:
+        client = redis.Redis(host=host, port=port, password=password)
+        response = client.ping()
+        return "Redis connection successful." if response else "Redis connection failed."
+    except Exception as e:
+        return f"Redis connection error: {e}"
 
-# Redis configuration
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT"))
+def check_postgres_connection(host, port, database, user, password):
+    try:
+        conn = psycopg2.connect(host=host, port=port, database=database, user=user, password=password)
+        cursor = conn.cursor()
+        cursor.execute('SELECT 1')
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return "PostgreSQL connection successful." if result and result[0] == 1 else "PostgreSQL connection failed."
+    except Exception as e:
+        return f"PostgreSQL connection error: {e}"
 
-# Connect to PostgreSQL
-conn = psycopg2.connect(
-    host=PG_HOST,
-    port=PG_PORT,
-    user=PG_USER,
-    password=PG_PASSWORD,
-    database=PG_DATABASE,
-)
+@app.route('/')
+def check_connections():
+    load_dotenv()
+    
+    # Redis configuration
+    redis_host = os.getenv("REDIS_HOST")
+    redis_port = int(os.getenv("REDIS_PORT"))
+    redis_password = os.getenv("REDIS_PASSWORD")
 
-# Connect to Redis
-redis_conn = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+    # PostgreSQL configuration
+    pg_host = os.getenv("PG_HOST")
+    pg_port = int(os.getenv("PG_PORT"))
+    pg_database = os.getenv("PG_DATABASE")
+    pg_user = os.getenv("PG_USER")
+    pg_password = os.getenv("PG_PASSWORD")
 
-def insert_user(name, email):
-    with conn:
-        with conn.cursor() as cursor:
-            cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
+    redis_result = check_redis_connection(redis_host, redis_port, redis_password)
+    postgres_result = check_postgres_connection(pg_host, pg_port, pg_database, pg_user, pg_password)
 
-def get_all_users():
-    with conn:
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM users")
-            return cursor.fetchall()
-
-def set_user_in_redis(user_id, name, email):
-    user_data = {"id": user_id, "name": name, "email": email}
-    redis_conn.hmset(f"user:{user_id}", user_data)
-
-def get_user_from_redis(user_id):
-    return redis_conn.hgetall(f"user:{user_id}")
+    return jsonify({
+        "redis_connection": redis_result,
+        "postgres_connection": postgres_result
+    })
 
 if __name__ == "__main__":
-    # Example usage
-    insert_user("John Doe", "john@example.com")
-    insert_user("Jane Smith", "jane@example.com")
-
-    all_users = get_all_users()
-    print("All Users:")
-    for user in all_users:
-        print(user)
-
-    user_id = 1
-    set_user_in_redis(user_id, "John Doe", "john@example.com")
-    user_data = get_user_from_redis(user_id)
-    print(f"User {user_id} from Redis:", user_data)
+    app.run(debug=True)
