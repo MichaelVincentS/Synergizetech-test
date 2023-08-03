5df1ca6b6b968df26e785960d44345aade69e36b
michaelvs.akseleran <michael.vincentius@akseleran.com>
2023-08-03 16:50:29 +0700

    Update terraform vpc

--- kubernetes-terraform/vpc.tf
+++ kubernetes-terraform/vpc.tf
@@ -1,6 +1,8 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
+variable "gke_name" {
+  description = "gke name"
+}
 variable "project_id" {
   description = "project id"
 }
@@ -16,13 +18,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "${var.gke_name}-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.gke_name}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
